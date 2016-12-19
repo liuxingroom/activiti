64cd96823e4b3288cc153494a71457018680a6bf
lx <2682204624@qq.com>
2016-12-19 17:54:57 +0800

    更新  静态内部类的方法

--- activiti/src/test/java/com/xing/single/Single.java
+++ activiti/src/test/java/com/xing/single/Single.java
@@ -4,14 +4,18 @@ package com.xing.single;
  *  单例设计模式（使用静态内部类） 
  */
 public class Single {
-	private Single(){}
-	public static  Single getInstance(){
+	//构造函数私有化
+	private Single (){}
+	
+	//对外提供的调用方法（获取Single对象）
+	public static Single getInstance(){
 		return Next.single;
 	}
 	
+	//静态内部类
 	static class Next{
 		private Next(){}
-		public static final Single single=new Single();
+		//真实创建Single对象的方法
+		private static Single single=new Single();
 	}
-	
 }
